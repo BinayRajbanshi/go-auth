f7cdc5dacfb199bba7cd6a9e20107439239fa511
Binay Rajbanshi <binay@squarebx.com>
2025-03-15 14:50:01 +0545

    mongodb setup

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -1,1 +1,41 @@
 package database
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error laoding .env file")
+	}
+
+	mongoUrl := os.Getenv("MONGODB_URL")
+
+	ctx, cancelCtx := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancelCtx()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoUrl))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	slog.Info("Monogo db connected successfully.")
+
+	return client
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("cluster-1").Collection(collectionName)
+	return collection
+}
